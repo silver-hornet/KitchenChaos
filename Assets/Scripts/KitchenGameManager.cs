c15d340347e32c78d3632c3ed734404aece11b81
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2023-09-27 19:48:00 -0400

    Working on Lecture 36. Game Start

---
+++ Assets/Scripts/KitchenGameManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenGameManager : MonoBehaviour
+{
+    enum State
+    {
+        WaitingToStart,
+        CountdownToStart,
+        GamePlaying,
+        GameOver,
+    }
+
+    State state;
+    float waitingToStartTimer = 1f;
+    float countdownToStartTimer = 3f;
+    float gamePlayingTimer = 10f;
+
+    void Awake()
+    {
+        state = State.WaitingToStart;
+    }
+
+    void Update()
+    {
+        switch (state)
+        {
+            case State.WaitingToStart:
+                waitingToStartTimer -= Time.deltaTime;
+                if (waitingToStartTimer < 0f)
+                {
+                    state = State.CountdownToStart;
+                }
+                break;
+            case State.CountdownToStart:
+                countdownToStartTimer -= Time.deltaTime;
+                if (countdownToStartTimer < 0f)
+                {
+                    state = State.GamePlaying;
+                }
+                break;
+            case State.GamePlaying:
+                gamePlayingTimer -= Time.deltaTime;
+                if (gamePlayingTimer < 0f)
+                {
+                    state = State.GameOver;
+                }
+                break;
+            case State.GameOver:
+                break;
+        }
+        Debug.Log(state);
+    }
+}
