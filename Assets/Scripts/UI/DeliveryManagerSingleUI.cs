f24587677ec2b07446a499cd50305cd5f1466808
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2023-09-27 15:39:03 -0400

    Finished Lecture 33. Delivery Manager UI

--- Assets/Scripts/UI/DeliveryManagerSingleUI.cs
+++ Assets/Scripts/UI/DeliveryManagerSingleUI.cs
@@ -2,13 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class DeliveryManagerSingleUI : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI recipeNameText; // Should use TextMeshProUGUI instead of just TextMeshPro. The former is meant for UI canvases.
+    [SerializeField] Transform iconContainer;
+    [SerializeField] Transform iconTemplate;
+
+
+    void Awake()
+    {
+        iconTemplate.gameObject.SetActive(false);
+    }
 
     public void SetRecipeSO(RecipeSO recipeSO)
     {
         recipeNameText.text = recipeSO.recipeName;
+
+        foreach (Transform child in iconContainer)
+        {
+            if (child == iconTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
+        foreach (KitchenObjectSO kitchenObjectSO in recipeSO.kitchenObjectSOList)
+        {
+            Transform iconTransform = Instantiate(iconTemplate, iconContainer);
+            iconTransform.gameObject.SetActive(true);
+            iconTransform.GetComponent<Image>().sprite = kitchenObjectSO.sprite;
+        }
     }
 }
