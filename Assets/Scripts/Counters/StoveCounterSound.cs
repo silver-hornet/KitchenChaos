925ab77f71abcfeca0f48bfdcb1b2be869666c1e
silver-hornet <70822422+silver-hornet@users.noreply.github.com>
2023-09-27 19:23:43 -0400

    Working on Lecture 35. Sound Effects

---
+++ Assets/Scripts/Counters/StoveCounterSound.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveCounterSound : MonoBehaviour
+{
+    [SerializeField] StoveCounter stoveCounter;
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        stoveCounter.OnStateChanged += StoveCounter_OnStateChanged;
+    }
+
+    private void StoveCounter_OnStateChanged(object sender, StoveCounter.OnStateChangedEventArgs e)
+    {
+        bool playSound = e.state == StoveCounter.State.Frying || e.state == StoveCounter.State.Fried;
+        if (playSound)
+        {
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.Pause();
+        }
+    }
+}
